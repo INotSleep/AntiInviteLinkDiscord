2437a35df9f113847762f5e0fd83fe7940cea7d3
INotSleep <77581131+INotSleep@users.noreply.github.com>
2021-09-12 20:03:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,13 @@
 let config = {}
 
 const config.token = "BOT_TOKEN"
+
 const config.immuneRoles = []
+
+const config.logChannelID = "CHANNEL_ID"
+const config.logGuildID = "GUILD_ID"
 const config.loggingEnabled = false
+const config.loggingIsEmbed = true
 
 // export data
 export { config }
