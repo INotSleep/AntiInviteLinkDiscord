2282d3eebc09d04afb32cbe492ee30d76616764a
INotSleep <77581131+INotSleep@users.noreply.github.com>
2021-09-12 16:51:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const client = new Discord.Client({
   intents: [ "GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_BANS", "GUILD_EMOJIS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING" ]
 })
 // import config
-import { token, immuneRoles } from "./config.js";
+import { config } from "./config.js";
+// import launge file
+import { messages } from "./launge.js"
 // check bot token
 if ( token == "BOT_TOKEN") {
   console.log("SET YOUR BOT TOKEN IN CONFIG.JS!!")
