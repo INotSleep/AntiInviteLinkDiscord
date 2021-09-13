b5acf99e41f0241203a27164a1f46311d2f0ed76
INotSleep <77581131+INotSleep@users.noreply.github.com>
2021-09-13 22:32:06 +0300

    Update launge.js

--- launge.js
+++ launge.js
@@ -1,22 +1,21 @@
 // setup data
-const messages = {}
-
-messages.deleteAlert = `{user}, invite links are not allowed!`
-
-messages.logging.embed.title = `Invite link was posted`
-messages.logging.embed.field1.name = `User:`
-messages.logging.embed.field1.value = `{user}`
-messages.logging.embed.field2.name = `Channel:`
-messages.logging.embed.field2.value = `{channel}`
-messages.logging.embed.field3.name = `Content:`
-messages.logging.embed.field3.value = `{content}`
-messages.logging.embed.footer.text = `AntiInviteLinkDiscord`
-messages.logging.embed.footer.imageEnabled = true
-messages.logging.embed.footer.image = `{botAvatar}`
-messages.logging.embed.timestamp = true
-messages.logging.embed.color = `#ffffff`
-
-messages.logging.noEmbed = `User:\n{user}\nChannel:\n{channel}\nContent:\n{content}`
+const messages = {
+  deleteAlert = `{user}, invite links are not allowed!`,
+  logging: {
+    embed: {
+      title:  `Invite link was posted`,
+      description: `User:\n{user}\nChannel:\n{channel}\nContent:{content}`,
+      footer: {
+        text: `AntiInviteLinkDiscord`,
+        imageEnabled: true,
+        image: `{botAvatar}`
+      },
+      timestamp: true,
+      color: `#ffffff`
+    },
+    noEmbed: `User:\n{user}\nChannel:\n{channel}\nContent:\n{content}`
+  }
+}
 
 // export data
 export { messages }
